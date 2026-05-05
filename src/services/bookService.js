cb198facf5720b722eae7fb8d5caa641a50b8fc4
zma8 <zainabmoosa880@gmail.com>
2026-05-05 08:43:26 +0300

    add auth service , bookservice , app.jsx

---
+++ src/services/bookService.js
@@ -0,0 +1,49 @@
+const BASE_URL=`${import.meta.env.VITE_BACK_END_SERVER_URL}/api/books`
+
+const getToken=()=>localStorage.getItem('token')
+
+const getAllBooks=async()=>{
+    const res=await fetch(BASE_URL,{
+        headers:{ Authorization:`Bearer${getToken()}`
+        },
+    })
+    const data=await res.json()
+    if(data.err){
+        throw new Error(data.err)
+    }
+    return data
+}
+
+const createBook=async(formData)=>{
+    const res=await fetch(BASE_URL,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            Authorization:`Bearer${getToken()}`,
+        },
+        body:JSON.stringify(formData),
+    })
+    const data=await res.json()
+    if(data.err){
+        throw new Error (data.err)
+    }
+    return data
+}
+
+const updateBook=async(id,formData)=>{
+    const res=await fetch(`${BASE_URL}/${id}`,{
+        method:'PUT',
+        headers:{
+            'Content-Type':'application/json',
+            Authorization:`Bearer${getToken()}`,
+        },
+        body:JSON.stringify(formData),
+    })
+    const data=await res.json()
+    if(data.err){
+        throw new Error (data.err)
+    }
+    return data
+}
+
+export{getAllBooks,createBook,updateBook}
